5decc75dc35f234376c1fc76462e13aae0523957
marianaWest <97693233+marianaWest@users.noreply.github.com>
2022-04-28 15:32:19 +0100

    responsive

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ document.querySelector('button').addEventListener('click', event => {
 //only works once
 
 const leon = {
-  leonSaysList: ["First tryyyyyyy", "We're baddies, we write bad code", "be a baddie", "blame it on the nuns", "go kiss some babies", "it's automagic", "thank you for the gifted subs", "we go get!", "don't get got", "you will make it to the other end", "it's a marathon, not a sprint", "you don't need to be a flexosaurus", "how's your Anki?", "get that spaced repetition", "Smurfs are here to listen", "I want a thousand", "who will carry the boats and logs?", "be Beyonce", "go through the trough of sorrow", "you are built different", "I wanna get into the trough of sorrow", "let me get spicy", "I'm here to get people jooobs and I'm f* good at it", "I don't care about the system", "you're a software engineer", "I want to burn those structures to the ground"],
+  leonSaysList: ["First tryyyyyyy", "We're baddies, we write bad code", "be a baddie", "blame it on the nuns", "go kiss some babies", "it's automagic", "thank you for the gifted subs", "we go get!", "don't get got", "you will make it to the other end", "it's a marathon, not a sprint", "you don't need to be a flexosaurus", "how's your Anki?", "get that spaced repetition", "Smurfs are here to listen", "I want a thousand", "who will carry the boats and logs?", "be Beyonce", "you are built different", "you will get through the trough of sorrow", "let me get spicy", "I'm here to get people jobs and I'm f* good at it", "I don't care about the system", "you're a software engineer", "I want to burn those structures to the ground"],
   leonSays: "",
 
   yatesShuffle(array) {
@@ -22,7 +22,7 @@ const leon = {
 },
 
 leonSays() {
-  this.leonSays = this.yatesShuffle(this.leonSaysList).slice(0,1)
-  document.getElementById("leon-speaks").innerHTML = `${this.leonSays[0]}`
+  this.leonSaysNow = this.yatesShuffle(this.leonSaysList).slice(0,1)
+  document.getElementById("leon-speaks").innerHTML = `${this.leonSaysNow[0]}`
 }
 }
